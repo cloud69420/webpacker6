d7d43684926d8a9c1ea279e77722409d6f7f4fdf
Your Name <you@example.com>
2021-04-11 03:51:14 +0200

    attempt to add jquery to webpacker configs

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,3 +1,12 @@
-const { webpackConfig } = require('@rails/webpacker')
+const { webpackConfig } = require("@rails/webpacker")
+const webpack = require("webpack")
+
+webpackConfig.plugins.append("Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  })
+)
 
 module.exports = webpackConfig
+
